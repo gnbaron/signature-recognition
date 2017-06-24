b2010d602ab81e5d307e4561147af43789436f2b
guilhermefloriani <floriani.guilherme@gmail.com>
2017-06-23 21:42:36 -0300

    Adjusting contrast, denoising and start to crop the image

--- sigrecog/__main__.py
+++ sigrecog/__main__.py
@@ -11,13 +11,28 @@ def main():
     inputFile = '001001_000.png'
     img = cv2.imread(os.path.join(inputFolder, inputFile), 0)
     
-    # constrast adjust
-    # img = np.float32(img) * (1.2 / 255)
+    # constrast
+    res = np.float32(img, dtype = np.uint8) * (1.2 / 255)
 
-    print(np.float32(img))
+    # denoising
+    img = cv2.fastNlMeansDenoising(img)
+
+    # binarization
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+    # crop blank space
+    img = crop(img)
+
+    print(img)
     cv2.imshow('image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def crop(image):
+    contours = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnt = contours[0]
+    x, y, w, h = cv2.boundingRect(cnt)
+    return image[y:y + h, x:x + w]
 
 def load_images(folder):
     images = []
