d7d7102eb55a055fcaadd206b407e8265db112cf
guilhermefloriani <floriani.guilherme@gmail.com>
2017-06-28 19:20:00 -0300

    Preprocessor functions

---
+++ preprocessor.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+
+def prepare(input):
+    # preprocessing the image input
+    clean = cv2.fastNlMeansDenoising(input)
+    ret, tresh = cv2.threshold(clean, 127, 255, cv2.THRESH_BINARY_INV)
+    img = crop(tresh)
+
+    # 40x10 image as a flatten array
+    flatten_img = cv2.resize(img, (40, 10), interpolation=cv2.INTER_AREA).flatten()
+
+    # resize to 400x100
+    resized = cv2.resize(img, (400, 100), interpolation=cv2.INTER_AREA)
+    columns = np.sum(resized, axis=0)  # sum of all columns
+    lines = np.sum(resized, axis=1)  # sum of all lines
+
+    h, w = img.shape
+    aspect = w / h
+
+    data = np.array([*flatten_img, *columns, *lines, aspect], dtype=float)
+    return np.reshape(data, (901, 1))
+
+
+def crop(img):
+    inverted = 255 - img
+    points = cv2.findNonZero(inverted)
+    x, y, w, h = cv2.boundingRect(points)
+    return img[y: y+h, x: x+w]
